4c90771d7c65f29edb25b57437f93fe39d46711a
Hasan Tareque <hmtareque@gmail.com>
2021-02-16 18:02:54 +0000

    jwt auth implemented

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+ //   Route::resource('products', 'ProductController');
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+    Route::resource('products', 'ProductController');
+
+});

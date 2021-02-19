089dc2439a9a8f865633a9941c0b2e4066e03189
Hasan Tareque <hmtareque@gmail.com>
2021-02-19 15:21:24 +0000

    users seeder updated

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = array(
+            'admin' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@shopx.com',
+            'password' => bcrypt('password')
+        );
+
+        User::create($user);
+
+        $fake = Faker\Factory::create();
+
+        for($u = 0; $u < 10; $u++) {
+            $user = array(
+                'name' => $fake->name,
+                'email' => $fake->safeEmail,
+                'password' => bcrypt('password')
+            );
+
+            User::create($user);
+        }
+    }
+}

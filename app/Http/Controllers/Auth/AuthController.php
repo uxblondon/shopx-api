73b92216b56ad5ae692eba1c118a7e8f64891f61
Hasan Tareque <hmtareque@gmail.com>
2021-02-24 10:47:56 +0000

    auth controller updated

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,15 +6,7 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-      //  $this->middleware('auth:api', ['except' => ['login']]);
-    }
+    
 
     /**
      * Get a JWT via given credentials.
@@ -25,7 +17,7 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
 
-        if (! $token = auth()->attempt($credentials)) {
+        if (!$token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 

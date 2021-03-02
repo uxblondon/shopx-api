ddae8ff1a6c4c3041ac6aa10352d6947a4d5b8e0
Hasan Tareque <hmtareque@gmail.com>
2021-03-02 17:32:10 +0000

    image update controller

--- app/Http/Controllers/Api/ProductImageController.php
+++ app/Http/Controllers/Api/ProductImageController.php
@@ -9,26 +9,6 @@ use Illuminate\Http\Request;
 class ProductImageController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -39,27 +19,7 @@ class ProductImageController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    
 
     /**
      * Update the specified resource in storage.

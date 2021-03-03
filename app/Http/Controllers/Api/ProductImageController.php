93c2a1b7c4c50336167f7209de2573dddf726267
Hasan Tareque <hmtareque@gmail.com>
2021-03-03 12:21:01 +0000

    product image store updated

--- app/Http/Controllers/Api/ProductImageController.php
+++ app/Http/Controllers/Api/ProductImageController.php
@@ -19,24 +19,32 @@ class ProductImageController extends Controller
      */
     public function store(Request $request, $product_id)
     {
-        $image = $request->file('image');
-        $location = str_replace('', '', 'products/'.$product_id.'/'.$image->getClientOriginalName());
 
-        Storage::disk('s3')->put($location, file_get_contents($request->file('image')));
+        try {
+            $image = $request->file('image');
+            $location = str_replace('', '', 'products/' . $product_id . '/' . $image->getClientOriginalName());
 
-        $image_data = array(
-            'product_id' => $product_id,
-            'description' => $request->get('description'),
-            'location' => Storage::url($location),
-            'feature_image' => $request->get('feature_image') ? 1 : 0,
-        );
+            Storage::disk('s3')->put($location, file_get_contents($request->file('image')));
 
-        $product_image = ProductImage::create($image_data);
+            $image_data = array(
+                'product_id' => $product_id,
+                'description' => $request->get('description'),
+                'location' => Storage::url($location),
+            );
 
-       return response()->json(['status' => 'success', 'data' => $product_image]);
+            if ($request->has('feature_image')) {
+                $image_data['feature_image'] = $request->get('feature_image') ? 1 : 0;
+            }
+
+            $product_image = ProductImage::create($image_data);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => 'Failed to upload product image.']);
+        }
+
+        return response()->json(['status' => 'success', 'message' => 'Product image updated successfully.', 'data' => $product_image]);
     }
 
-    
+
 
     /**
      * Update the specified resource in storage.

315ef4cae4ac015057608d3b093b35a349ac0002
Hasan Tareque <hmtareque@gmail.com>
2021-03-03 10:18:31 +0000

    config updated

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,10 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => 'AKIARNR7HJGFVZ6VFDE6',
-            'secret' => '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',
-            'region' => 'eu-west-2',
-            'bucket' => 'shopx-api-images',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
         ],

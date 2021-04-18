bac4ac254e803d90b9f4cadcef73794a9fbaeb30
Hasan Tareque <hmtareque@gmail.com>
2021-04-18 15:09:00 +0100

    order address update

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use DB;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\OrderDelivery;
 
 class OrderController extends Controller
 {
@@ -33,11 +34,11 @@ class OrderController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function ref()
+    public function ref($shipping_method, $no_of_items)
     {
         
             $no_of_orders = Order::where('created_at', '>=', date('Y-m-d').' 00:00:00')->where('created_at', '<=', date('Y-m-d').' 59:59:59')->count();
-            $sequence = date('ymd').str_pad($no_of_orders+1, 5, '0', STR_PAD_LEFT);
+            $sequence = date('ymd').str_pad($no_of_orders+1, 5, '0', STR_PAD_LEFT).strtoupper($shipping_method[0]).$no_of_items;
       
        
         return $sequence;
@@ -61,13 +62,137 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
+        DB::beginTransaction();
+        try {
+
+        } catch(\Exception $e) {
+
+        }
         $customer = $request->get('customer');
-        $order = array(
-            'ref' => $this->ref(),
+        $basket_items = $request->get('basket_items');
+        $shipping = $request->get('shipping');
+        $payment = $request->get('payment');
+
+        $order_data = array(
+            'ref' => $this->ref($shipping['method'], count($basket_items)),
             'name' => $customer['name'],
+            'email' => $customer['email']
         );
 
-        return $order;
+        $order = Order::create($order_data);
+
+        $order_items = [];
+        foreach($basket_items as $item) {
+            $order_item = array(
+                'order_id' => $order->id,
+                'product_id' => $item['product_id'],
+                'variant_id' => $item['id'],
+                'title' => $item['title'],
+                'width' => $item['width'],
+                'length' => $item['length'],
+                'height' => $item['height'],
+                'weight' => $item['weight'],
+                'separated_shipping_required' => $item['separated_shipping_required'],
+                'additional_shipping_cost' => $item['additional_shipping_cost'],
+                'variant_1_name' => $item['variant_1_name'],
+                'variant_1_value' => $item['variant_1_value'],
+                'variant_2_name' => $item['variant_2_name'],
+                'variant_2_value' => $item['variant_2_value'],
+                'variant_3_name' => $item['variant_3_name'],
+                'variant_3_value' => $item['variant_3_value'],
+                'price' => $item['price'],
+                'quantity' => $item['quantity'],
+            );
+
+            OrderItem::create($order_item);
+        }
+
+        if($shipping['method'] === 'delivery') {
+
+            $delivery_address = $shipping['delivery_address'];
+
+            $order_delivery_address = array(
+                'order_id' => $order->id,
+                'type' => 'delivery',
+                'name' => $delivery_address['name'],
+                'address_line_1' => $delivery_address['address_line_1'],
+                'address_line_2' => $delivery_address['address_line_2'],
+                'city' => $delivery_address['city'],
+                'county' => $delivery_address['county'],
+                'postcode' => $delivery_address['postcode'],
+                'country_code' => $delivery_address['country'],
+                'note' => $delivery_address['note']
+            );
+
+            $deliveries = $shipping['deliveries'];
+
+            foreach($deliveries as $delivery) {
+
+                $delivery_option = $delivery['option'];
+
+                $order_delivery_data = array(
+                    'order_id' => $order->id,
+                    'method' => 'delivery',
+                    'provider' => $delivery_option['provider'],
+                    'service' => $delivery_option['service'],
+                    'speed' => $delivery_option['speed'],
+                    'cost' => $delivery_option['cost'],
+                );
+
+                $order_delivery = OrderDelivery::create($order);
+
+                $order_delivery_address['order_delivery_id'] = $order_delivery->id;
+                
+                
+
+                
+
+                $delivery_items = [];
+                $items = $delivery['items'];
+                foreach($items as $item) {
+                    //find order item id 
+                    // order_id variant_id product_id 
+
+                    $delivery_items[] = array(
+                        'delivery_id' => '',
+                        'order_item_id' => '',
+                    );
+                }
+            }
+
+
+        } elseif($shipping['method'] === 'collection') {
+
+
+
+            $order_address = array(
+                'type' => 'collection',
+                'name' => '',
+                'address_line_1' => '',
+                'address_line_2' => '',
+                'city' => '',
+                'county' => '',
+                'postcode' => '',
+                'country_code' => '',
+                'note' => ''
+            );
+
+            $order_delivery = array(
+                'method' => 'collection',
+                'order_address_id' => 'test',
+                'provider' => '',
+                'service' => '',
+                'speed' => '',
+                'cost' => '',
+            );
+
+        }
+
+
+
+        return $shipping;
+
+       // return $request->all();
     }
 
     /**

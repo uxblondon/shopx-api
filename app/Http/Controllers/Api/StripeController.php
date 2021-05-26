22382befeae5b68786e451dd03ee50968d41b570
Hasan Tareque <hmtareque@gmail.com>
2021-05-27 00:43:27 +0100

    payment notifications test

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -65,9 +65,10 @@ class StripeController extends Controller
             $type = $data->type;
            // if ($type == 'payment_intent.succeeded') {
                 $payment = $data->data->object;
-                if ($payment->charges->data[0]->status == 'succeeded') {
+                $status = $payment->charges->data[0]->status;
+                if ($payment->charges->data[0]->status === 'succeeded') {
                     OrderPayment::where('payment_id', $payment->id)
-                    ->update(['payment_status' => $payment->charges->data->status, 'payment_confirmed' => 1, 'updated_at' => date('Y-m-d H:i:s')]);
+                    ->update(['payment_status' => $status, 'payment_confirmed' => 1, 'updated_at' => date('Y-m-d H:i:s')]);
                 }
 
                 DB::table('test')->insert(['data' => $type]);
@@ -82,7 +83,7 @@ class StripeController extends Controller
             );
         }
 
-        return response()->json(['msg' => 'stripe payment hook', 'type' => $type, 'id' => $payment->id, 'status' => $payment->charges->data[0]->status], 200);
+        return response()->json(['msg' => 'stripe payment hook', 'type' => $type, 'id' => $payment->id, 'status' => $status], 200);
     }
 }
 
